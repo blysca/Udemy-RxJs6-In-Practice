65b298052f35a5cf5f3eeca83bfa1ccfbfed25ed
Serhii Plyska <blysca@newtonideas.com>
2019-09-25 16:04:34 +0300

    Completed: "Section 2: 20. The RxJs exhaustMap Operator - In-Depth Explanation"

--- src/rxjs-course-1-operators/src/app/about/about.component.ts
+++ src/rxjs-course-1-operators/src/app/about/about.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import {fromEvent, interval, Observable, timer} from 'rxjs';
+import {Component, OnInit} from '@angular/core';
+import {concat, interval, merge, of} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'about',
@@ -7,31 +8,25 @@ import {fromEvent, interval, Observable, timer} from 'rxjs';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-    const http$ = new Observable( observer => {
-      fetch('/api/courses')
-        .then( response => {
-          return response.json();
-        })
-        .then( body => {
-          observer.next(body);
-          observer.complete();
-        })
-        .catch(error => {
-          observer.error(error);
-        })
-    });
-    
-    
-    http$.subscribe(
-      courses => console.log('*** courses ', courses),
-      error => console.log('*** error ', error), // noop,
-      () => console.log('*** completed ')
-      
-    )
+  
+  constructor() {
   }
-
+  
+  ngOnInit() {
+    const source1$ = interval(1000);
+    const source2$ = source1$.pipe(map(val => 10* val));
+    const source3$ = of('a', 'k', 'x');
+    const source4$ = of(7, 8, 9);
+    
+    const result$ = merge(source1$, source2$);
+    
+    /*result$.subscribe(
+      value => console.log('*** value = ', value),
+      error => console.log('*** value = ', error),
+      () => console.log('*** completed '),
+    );*/
+    
+    result$.subscribe(console.log)
+  }
+  
 }
--- src/rxjs-course-1-operators/src/app/home/home.component.ts
+++ src/rxjs-course-1-operators/src/app/home/home.component.ts
@@ -1,25 +1,53 @@
 import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {interval, Observable, of, timer} from 'rxjs';
-import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
+import {interval, noop, Observable, of, timer} from 'rxjs';
+import {catchError, delayWhen, map, filter, shareReplay, tap} from 'rxjs/operators';
+
+import {createHttpObservable} from '../common/util';
+import {Course} from '../model/course';
+
 
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-
-    constructor() {
-
-    }
-
-    ngOnInit() {
-
-
-
-    }
-
+  beginnersCourses$: Observable<Course[]>;
+  advancedCourses$: Observable<Course[]>;
+  
+  ngOnInit() {
+    const http$ = createHttpObservable('/api/courses');
+    
+    const courses$ = http$
+      .pipe(
+        tap(() => console.log('*** Http request executed')),
+        map(res => Object.values(res['payload'])),
+        shareReplay()
+      );
+    
+    this.beginnersCourses$ = courses$.pipe(
+      map((courses: Course[]) => {
+          return courses.filter(course => course.category == 'BEGINNER');
+        }
+      )
+    );
+    
+    this.advancedCourses$ = courses$.pipe(
+      map((courses: Course[]) => {
+          return courses.filter(course => course.category == 'ADVANCED');
+        }
+      )
+    );
+    
+    
+    courses$.subscribe(
+      (courses: Course[]) => {
+        console.log('*** courses ', courses);
+      },
+      noop, // error => console.log('*** error ', error),
+      () => console.log('*** completed ')
+    );
+  }
+  
 }
