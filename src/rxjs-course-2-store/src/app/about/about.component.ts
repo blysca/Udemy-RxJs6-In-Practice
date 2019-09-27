38378035f5e9dc2dd4f57d255d8933fc9ed5011d
Serhii Plyska <blysca@newtonideas.com>
2019-09-27 18:32:29 +0300

    Completed: "Section 5: RxJs Subjects and the Store Pattern"

---
+++ src/rxjs-course-2-store/src/app/about/about.component.ts
@@ -0,0 +1,40 @@
+import {Component, OnInit} from '@angular/core';
+import {
+  AsyncSubject, ReplaySubject
+} from 'rxjs';
+
+
+@Component({
+  selector: 'about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+  
+  ngOnInit() {
+    const subject = new ReplaySubject();
+    const series$ = subject.asObservable();
+    
+    series$.subscribe( val =>  console.log('first sub: ', val));
+    
+    subject.next(1);
+    subject.next(2);
+    subject.next(3);
+    
+      // subject.complete();
+    
+    setTimeout(() =>{
+      series$.subscribe( val =>  console.log('second sub: ', val));
+      subject.next(4);
+    }, 3000)
+    
+  }
+  
+  
+}
+
+
+
+
+
+
