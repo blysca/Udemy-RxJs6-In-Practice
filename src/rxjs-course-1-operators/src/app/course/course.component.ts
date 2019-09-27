9b5ffb98966aa295cf22348bdc585d4f7fc81f80
Serhii Plyska <blysca@newtonideas.com>
2019-09-27 16:46:41 +0300

    Completed: "Section 4: Building a RxJs Custom Operator"

--- src/rxjs-course-1-operators/src/app/course/course.component.ts
+++ src/rxjs-course-1-operators/src/app/course/course.component.ts
@@ -1,8 +1,17 @@
 import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Course} from '../model/course';
-import {debounceTime, distinctUntilChanged, map, startWith, switchMap} from 'rxjs/operators';
-import {fromEvent, Observable} from 'rxjs';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  startWith,
+  switchMap,
+  tap,
+  throttle,
+  throttleTime
+} from 'rxjs/operators';
+import {forkJoin, fromEvent, Observable} from 'rxjs';
 import {Lesson} from '../model/lesson';
 import {createHttpObservable} from '../common/util';
 import {debug, RxJsLoggingLevel, setRxJsLoggingLevel} from '../common/debug';
@@ -29,25 +38,27 @@ export class CourseComponent implements OnInit, AfterViewInit {
   ngOnInit() {
     this.courseId = this.route.snapshot.params['id'];
     
-    this.course$ = createHttpObservable(`/api/courses/${this.courseId}`)
-      .pipe(
-        debug( RxJsLoggingLevel.INFO, 'course value  ')
-      );
+    const course$ = createHttpObservable(`/api/courses/${this.courseId}`);
     
-    setRxJsLoggingLevel(RxJsLoggingLevel.TRACE);
+    const lessons$ = this.loadLessons();
+    
+    forkJoin(course$, lessons$)
+      .pipe(
+        tap(([course, lessons]) => {
+          console.log('*** course ', course);
+          console.log('*** lessons ', lessons);
+        })
+      )
+      .subscribe();
   }
   
   ngAfterViewInit() {
-    this.lessons$ = fromEvent<any>(this.input.nativeElement, 'keyup')
+    fromEvent<any>(this.input.nativeElement, 'keyup')
       .pipe(
         map((event: any) => event.target.value),
-        startWith(''),
-        debug(RxJsLoggingLevel.TRACE, 'search '),
-        debounceTime(400),
-        distinctUntilChanged(),
-        switchMap(search => this.loadLessons(search)),
-        debug(RxJsLoggingLevel.DEBUG, 'lessons value'),
-      );
+        throttleTime(500),
+      )
+      .subscribe(console.log);
   }
   
   loadLessons(search = ''): Observable<Lesson[]> {
